65a25667040f4461ed03303b7d04c38b40c54a67
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2018-01-01 16:05:39 +0100

    Initial commit

---
+++ build/CMakeFiles/pizzaOs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pizzaOs.dir/src/main.c.o"
+  "pizzaOs.pdb"
+  "pizzaOs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pizzaOs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
