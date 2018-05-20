3218c96caf9b14de87b6cd8178141715216aabfa
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2018-05-20 20:53:39 +0200

    Adjust scheduler preempt rate.

--- src/kernel/sched.c
+++ src/kernel/sched.c
@@ -16,12 +16,8 @@ unsigned long long time_since_last_switch = 0;
 void sched_notify(irq_t* irq, unsigned long long interval)
 {
 	time_since_last_switch += interval;
-	//kernel_log("Time: %d", time_since_last_switch);
-	//if (time_since_last_switch > 0)
-	//if (true)
-	if (time_since_last_switch > 2000000)
+	if (time_since_last_switch > 20000)
 	{
-		kernel_log("Interval: %l, time_since_last_switch: %l", interval, time_since_last_switch);
 		time_since_last_switch = 0;
 		interrupt_finish(irq);
 		thread_leave();
