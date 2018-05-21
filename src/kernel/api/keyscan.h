61c2070705b729a544861e72515ae3724f25a959
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2018-05-21 14:51:28 +0200

    Add a few scancodes

---
+++ src/kernel/api/keyscan.h
@@ -0,0 +1,10 @@
+/**
+ * Contains keyboard scancode definitions
+ */
+
+#ifndef API_KEYSCAN_H_
+#define API_KEYSCAN_H_
+
+
+
+#endif /* API_KEYSCAN_H_ */
---
+++ src/kernel/bus/keyboard.h
@@ -0,0 +1,13 @@
+/*
+ * keyboard.h
+ *
+ *  Created on: May 21, 2018
+ *      Author: seeseemelk
+ */
+
+#ifndef BUS_KEYBOARD_H_
+#define BUS_KEYBOARD_H_
+
+
+
+#endif /* BUS_KEYBOARD_H_ */
