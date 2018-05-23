d06a28328aa4f3eb41db37b2dd6e2c3edd47a85e
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2018-05-23 11:38:50 +0200

    Fix 'm' key

--- src/kernel/api/keymaps/nlBE.h
+++ src/kernel/api/keymaps/nlBE.h
@@ -14,10 +14,15 @@ static char keymapNormal[256] = {
 		[KB_A] = 'q', [KB_B] = 'b', [KB_C] = 'c', [KB_D] = 'd',
 		[KB_E] = 'e', [KB_F] = 'f', [KB_G] = 'g', [KB_H] = 'h',
 		[KB_I] = 'i', [KB_J] = 'j', [KB_K] = 'k', [KB_L] = 'l',
-		[KB_M] = 'm', [KB_N] = 'n', [KB_O] = 'o', [KB_P] = 'p',
+		[KB_M] = ',', [KB_N] = 'n', [KB_O] = 'o', [KB_P] = 'p',
 		[KB_Q] = 'a', [KB_R] = 'r', [KB_S] = 's', [KB_T] = 't',
 		[KB_U] = 'u', [KB_V] = 'v', [KB_W] = 'z', [KB_X] = 'x',
-		[KB_Y] = 'y', [KB_Z] = 'w'
+		[KB_Y] = 'y', [KB_Z] = 'w', [KB_SEMICOLON] = 'm',
+
+		/*
+		[KB_1] = '&', [KB_2] = 'é', [KB_3] = '"', [KB_4] = '\'', [KB_5] = '(',
+		[KB_6] = '§', [KB_7] = 'è', [KB_8] = '!', [KB_9] = 'ç',  [KB_0] = 'à'
+		*/
 };
 
 #endif /* API_KEYMAPS_NLBE_H_ */
